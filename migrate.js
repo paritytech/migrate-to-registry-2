f0e4a387d6fdc74cce07851452eb2bea5139edc7
Jannis R <mail@jannisr.de>
2017-02-01 19:02:00 +0100

    query fee for registering

--- migrate.js
+++ migrate.js
@@ -3,7 +3,9 @@
 const fs = require('fs')
 const path = require('path')
 const co = require('co')
-const callContract = require('./lib/call-contract')
+const api = require('./lib/api')
+const {fromWei} = require('@parity/parity.js').Api.util
+const postToContract = require('./lib/post-to-contract')
 const waitForConfirmations = require('./lib/wait-for-confirmations')
 
 const abi = JSON.parse(fs.readFileSync(path.join(__dirname, 'contracts/SimpleRegistry.abi'), {encoding: 'utf8'}))
@@ -13,17 +15,21 @@ const setData = abi.find((item) => item.name === 'setData')
 const migrate = co.wrap(function* (registryAddress, registryOwner, data) {
   const {names} = data // todo: reverses, proposedReverses
 
+  const registry = api.newContract(abi, registryAddress)
+  const fee = yield registry.instance.fee.call({}, [])
+  console.info('fee is', fromWei(fee).toNumber(), 'ETH')
+
   for (let nameHash in names) {
     const {data} = names[nameHash] // todo: owner
 
-    const tx1 = yield callContract(registryOwner, registryAddress, reserve, [nameHash], 1)
+    const tx1 = yield postToContract(registryOwner, registryAddress, reserve, [nameHash], fee)
     yield waitForConfirmations(tx1)
     console.info('registered', nameHash, '–', tx1)
 
     for (let key in data) {
       const value = data[key]
 
-      const tx = yield callContract(registryOwner, registryAddress, setData, [nameHash, key, value])
+      const tx = yield postToContract(registryOwner, registryAddress, setData, [nameHash, key, value])
       yield waitForConfirmations(tx)
       console.info('\t', key, '->', value, '–', tx)
     }
